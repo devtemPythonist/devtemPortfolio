22e121658069cdff7cfe572f05c1b033194dcc4f
Temurmirzo Abdukarimov <114911047+devtemPythonist@users.noreply.github.com>
2023-02-17 12:09:00 +0500

    changedFooter

--- src/components/shared/FooterCopyright.vue
+++ src/components/shared/FooterCopyright.vue
@@ -16,7 +16,7 @@ export default {
 			class="font-general-regular text-lg text-ternary-dark dark:text-ternary-light"
 		>
 			&copy; {{ copyrightDate }}.
-			. Developed by
+			Developed by
 			<a
 				target="__blank"
 				class="font-general-medium text-secondary-dark dark:text-secondary-light uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 duration-500"
